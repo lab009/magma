39597f614ed29b7bb2d8858f266d566ae4993207
Oleg Orlov <mail@orlovoleg.com>
2017-02-28 16:26:41 +0300

    Add boot for run babel in node

--- packages/magma-scripts/bin/register.js
+++
@@ -1,7 +0,0 @@
-require('@lab009/babel-preset-magma');
-
-require('babel-core/register')({
-  "presets": [
-    [require.resolve('@lab009/babel-preset-magma'), { "targets": { "node": "current" }, "modules": "commonjs" }],
-  ]
-});
