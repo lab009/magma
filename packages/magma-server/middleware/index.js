6bb28e467ed6ead53857927f56be9cedb1ffe13b
Oleg Orlov <mail@orlovoleg.com>
2017-03-07 16:25:12 +0300

    Add magma-server package

---
+++ packages/magma-server/middleware/index.js
@@ -0,0 +1,15 @@
+import createReactSSR from './reactApplication'
+import security from './security'
+import clientBundle from './clientBundle'
+import serviceWorker from './serviceWorker'
+import offlinePage from './offlinePage'
+import errorHandlers from './errorHandlers'
+
+export default {
+  createReactSSR,
+  security,
+  clientBundle,
+  serviceWorker,
+  offlinePage,
+  errorHandlers,
+}
