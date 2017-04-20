63a6472bab0e5269a6605e29e042383444506cd1
Oleg Orlov <mail@orlovoleg.com>
2017-04-20 12:09:26 +0300

    Add test script (#37)

--- packages/magma-scripts/bin/register.js
+++ packages/magma-scripts/bin/register.js
@@ -1,6 +1,9 @@
-require('@lab009/babel-preset-magma')
+/* eslint semi: ["error", "always"] */
+/* eslint comma-dangle: ["error", "never"] */
+
+require('@lab009/babel-preset-magma');
 
 require('babel-core/register')({
   presets: [[require.resolve('@lab009/babel-preset-magma'), { targets: { node: 'current' }, modules: 'commonjs' }]],
-  only: /(@lab009\/magma-|config\/values\.js$)/,
-})
+  only: /(@lab009\/magma-|config\/values\.js$)/
+});
---
+++ packages/magma-scripts/jest/cssTransform.js
@@ -0,0 +1,15 @@
+/* eslint semi: ["error", "always"] */
+/* eslint comma-dangle: ["error", "never"] */
+
+// This is a custom Jest transformer turning style imports into empty objects.
+// http://facebook.github.io/jest/docs/tutorial-webpack.html
+
+module.exports = {
+  process() {
+    return 'module.exports = {};';
+  },
+  getCacheKey() {
+    // The output is always the same.
+    return 'cssTransform';
+  }
+};
