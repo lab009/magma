450cac35d737aee6ec35af442a55e1c67cebb3a2
Oleg Orlov <mail@orlovoleg.com>
2017-05-04 12:12:02 +0300

    Update dependencies (#43)

--- packages/magma-scripts/client/AppContainer.js
+++
@@ -1,9 +0,0 @@
-import { Children } from 'react'
-
-// We create this wrapper so that we only import react-hot-loader for a
-// development build.  Small savings. :)
-const AppContainer = process.env.NODE_ENV === 'development'
-  ? require('react-hot-loader').AppContainer
-  : ({ children }) => Children.only(children)
-
-export default AppContainer
